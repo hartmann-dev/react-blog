ae446122af27a4942065094417abd07b4ac89670
Harald Hartmann <hh@include-once.de>
2018-12-13 14:16:04 +0100

    Lesson 156

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    seleectedPostId: null
   };
 
   componentDidMount() {
@@ -22,16 +23,27 @@ class Blog extends Component {
     });
   }
 
+  postSelectedHandler = id => {
+    this.setState({ seleectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.seleectedPostId} />
         </section>
         <section>
           <NewPost />
