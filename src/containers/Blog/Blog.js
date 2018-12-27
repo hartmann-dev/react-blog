b01177cbba2f7acda0f30cbd966d007ac97e89db
Harald Hartmann <hh@include-once.de>
2018-12-27 14:42:59 +0100

    Lesson 193

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Route, NavLink } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
+import FullPost from "./FullPost/FullPost";
+
 import NewPost from "./NewPost/NewPost";
 
 import "./Blog.css";
@@ -43,6 +45,7 @@ class Blog extends Component {
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" exact component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
