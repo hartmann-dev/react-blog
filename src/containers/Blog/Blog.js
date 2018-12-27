5e9b57445ee7a01659fa0048103dfa584dddd33e
Harald Hartmann <hh@include-once.de>
2018-12-27 14:08:52 +0100

    Lesson 186

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+
 import "./Blog.css";
 
 class Blog extends Component {
@@ -23,6 +25,7 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
       </div>
     );
   }
