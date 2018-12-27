d7731f86b906c254816948c566798914da5db3bf
Harald Hartmann <hh@include-once.de>
2018-12-27 20:41:33 +0100

    Lesson 201

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
 
@@ -44,7 +44,9 @@ class Blog extends Component {
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Switch>
           <Route path="/new-post" exact component={NewPost} />
-          <Route path="/posts/" component={Posts} />
+          <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="posts" />
+          {/* <Route path="/posts/" component={Posts} /> */}
         </Switch>
       </div>
     );
