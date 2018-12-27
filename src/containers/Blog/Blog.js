263acf1d51f57f8e4fe0560a88086496a55a4849
Harald Hartmann <hh@include-once.de>
2018-12-27 13:44:37 +0100

    Lesson 181

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 //import axios from "axios";
 import axios from "../../axios";
 
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -76,12 +73,6 @@ class Blog extends Component {
           </nav>
         </header>
         <section className="Posts"> {posts} </section>
-        <section>
-          <FullPost id={this.state.seleectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     );
   }
