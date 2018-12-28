199b5aa9ed56117d1a75266c30b588ffe9793bc6
Harald Hartmann <hh@include-once.de>
2018-12-28 09:26:50 +0100

    Lesson 204

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,6 +8,10 @@ import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    auth: false
+  };
+
   render() {
     return (
       <div className="Blog">
@@ -43,7 +47,9 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Switch>
-          <Route path="/new-post" exact component={NewPost} />
+          {this.state.auth ? (
+            <Route path="/new-post" exact component={NewPost} />
+          ) : null}
           <Route path="/posts" component={Posts} />
           <Redirect from="/" to="posts" />
           {/* <Route path="/posts/" component={Posts} /> */}
