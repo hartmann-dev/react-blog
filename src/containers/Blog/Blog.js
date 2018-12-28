e6ca9e3d246f83262a74112fc8bdb9a233909a6e
Harald Hartmann <hh@include-once.de>
2018-12-28 09:49:04 +0100

    Lesson 206

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,13 +3,17 @@ import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
 
-import NewPost from "./NewPost/NewPost";
-
+//import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asynComponent";
 import "./Blog.css";
+
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   };
 
   render() {
@@ -48,7 +52,7 @@ class Blog extends Component {
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" exact component={NewPost} />
+            <Route path="/new-post" exact component={AsyncNewPost} />
           ) : null}
           <Route path="/posts" component={Posts} />
           <Route render={() => <h1>nix gefunden</h1>} />
